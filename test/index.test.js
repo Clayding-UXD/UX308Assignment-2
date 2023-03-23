a2edca6ed8f7cbc659d7eda101d6d663ff20f881
Clayton Harding <croutonic5627@gmail.com>
2023-03-23 13:15:58 -0400

    change copyright

---
+++ test/index.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { describe, it, expect } from 'vitest';
+import { renderToString } from 'react-dom/server';
+import Index from '../pages/index.page.mdx';
+import { load } from 'cheerio';
+
+describe('index test', function () {
+  it('tests for h1 of coming soon', function () {
+    const sHtml = renderToString(React.createElement(Index));
+    const $ = load(sHtml);
+    expect($('h1').html()).toBe('Coming Soon');
+  });
+
+it('tests better', function () {
+  const sHtml = renderToString(React.createElement(Index));
+  expect(sHtml).toContain('better');
+});
+it('tests copyright', function () {
+  const sHtml = renderToString(React.createElement(Index));
+  expect(sHtml).toContain('Clayton Harding');
+});
+
+
+});
